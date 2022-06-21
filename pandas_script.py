8b41a5fabf3831810cb90418baa34f4f5ad7d4f2
agusrave <100858494+agusrave@users.noreply.github.com>
2022-06-21 20:04:01 +0200

    update functions file name

--- pandas_script.py
+++ pandas_script.py
@@ -3,5 +3,5 @@ Created on Mon Jun 20 14:13:12 2022
 
 @author: Agustina Ravettino
 """
-from pandas_script import pandas_function
+from functions import pandas_function
 pandas_function('directory_stats.txt')
